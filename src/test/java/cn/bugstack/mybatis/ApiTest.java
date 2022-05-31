7e593f286a2d9b448ae8447cf53093d729877cea
MacLi <macli1009@163.com>
2022-05-31 08:04:26 +0800

    init

---
+++ src/test/java/cn/bugstack/mybatis/ApiTest.java
@@ -0,0 +1,21 @@
+package cn.bugstack.mybatis;
+
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+import org.junit.Test;
+
+public class ApiTest {
+
+    private Logger logger = LoggerFactory.getLogger(ApiTest.class);
+
+    @Test
+    public void test_MapperProxyFactory(){
+
+    }
+
+    @Test
+    public void test_proxy_class(){
+
+    }
+
+}
---
+++ src/test/java/cn/bugstack/mybatis/test/dao/IUserDao.java
@@ -0,0 +1,4 @@
+package cn.bugstack.mybatis.test.dao;
+
+public interface IUserDao {
+}
