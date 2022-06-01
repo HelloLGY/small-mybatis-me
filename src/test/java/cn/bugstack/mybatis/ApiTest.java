4a97a4110ce7275810f0be721c3378a5351f4ad6
MacLi <macli1009@163.com>
2022-06-02 07:47:12 +0800

    创建简单的映射器代理工厂

--- src/test/java/cn/bugstack/mybatis/ApiTest.java
+++ src/test/java/cn/bugstack/mybatis/ApiTest.java
@@ -1,8 +1,12 @@
 package cn.bugstack.mybatis;
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
+import cn.bugstack.mybatis.binding.MapperProxyFactory;
+import cn.bugstack.mybatis.test.dao.IUserDao;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
 
 public class ApiTest {
 
@@ -10,6 +14,15 @@ public class ApiTest {
 
     @Test
     public void test_MapperProxyFactory(){
+        MapperProxyFactory<IUserDao> factory = new MapperProxyFactory<>(IUserDao.class);
+        HashMap<String, String> sqlSession = new HashMap<>();
+        sqlSession.put("cn.bugstack.mybatis.test.dao.IUserDao.queryUserName", "模拟执行 Mapper.xml 中 SQL 语句的操作：查询用户姓名");
+        sqlSession.put("cn.bugstack.mybatis.test.dao.IUserDao.queryUserAge", "模拟执行 Mapper.xml 中 SQL 语句的操作：查询用户年龄");
+        IUserDao iUserDao = factory.newInstance(sqlSession);
+        String res = iUserDao.queryUserName("10001");
+        logger.info("测试结果：{}", res);
+
+
 
     }
 
--- src/test/java/cn/bugstack/mybatis/test/dao/IUserDao.java
+++ src/test/java/cn/bugstack/mybatis/test/dao/IUserDao.java
@@ -1,4 +1,7 @@
 package cn.bugstack.mybatis.test.dao;
 
 public interface IUserDao {
+    String queryUserName(String uId);
+
+    Integer queryUserAge(String uId);
 }
